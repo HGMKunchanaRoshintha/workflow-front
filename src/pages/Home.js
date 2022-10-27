9a01abf7a1901efa1ba3e0154f6cbdb7919031c2
Kunchana <hgmkunchana@gmail.com>
2022-10-27 22:25:34 +0530

    finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,24 +35,18 @@ export default function Home() {
   // remove the root node details and set other data to return data refernce
   const returnData = ascendingByParentCard.slice(1);
 
-
   const list = [];
   returnData.forEach((product) => {
-    
-    list.push(product)
-  })
-
-
-
+    list.push(product);
+  });
 
   // this treeNodeSet const use for set the datas to tree node elements
   const treeNodeSet = returnData.map((item, index) => {
     // returnData.forEach(element => {
     //   element.parentCard == item.parentCard? <li></li> : <li></li>;
-    // });   
-    
-    return (      
-      rootNode.id == returnData.parentCard ?
+    // });
+
+    return rootNode.id == item.parentCard ? (
       <TreeNode
         key={item.id}
         label={
@@ -92,10 +86,10 @@ export default function Home() {
             </div>
           </div>
         }
-      >    
-
-        <TreeNode
-        key={item.id++}
+      ></TreeNode>
+    ) : (
+      <TreeNode
+        key={item.id}
         label={
           <div className="d-flex justify-content-center">
             <div className="card bg-primary">
@@ -133,45 +127,9 @@ export default function Home() {
             </div>
           </div>
         }
-      ></TreeNode>
+      >
         
-      </TreeNode>: <TreeNode label={
-          <div className="d-flex justify-content-center">
-            <div className="card bg-primary">
-              <ul
-                className="list-group bg-primary"
-                style={{ textAlign: "left" }}
-              >
-                <li className="list-group-item">
-                  <span style={{ fontWeight: "bold" }}>Id:</span> {item.id}
-                </li>
-                <li className="list-group-item">
-                  <span style={{ fontWeight: "bold" }}>Name:</span> {item.name}{" "}
-                </li>
-                <li className="list-group-item">
-                  <span style={{ fontWeight: "bold" }}>Description:</span>{" "}
-                  {item.description}
-                </li>
-                <li className="list-group-item">
-                  <span style={{ fontWeight: "bold" }}>Time Duration:</span>
-                  {item.timeDuration}
-                </li>
-                <li className="list-group-item">
-                  <span style={{ fontWeight: "bold" }}>parentCard:</span>{" "}
-                  {item.parentCard}
-                </li>
-                <li className="list-group-item">
-                  <span style={{ fontWeight: "bold" }}>Attributes:</span>
-                  {item.attributes}
-                </li>
-                <li className="list-group-item">
-                  <span style={{ fontWeight: "bold" }}>Role:</span>
-                  {item.role}
-                </li>
-              </ul>
-            </div>
-          </div>
-        }></TreeNode>
+      </TreeNode>
     );
   });
 
